08b5b735694b5d16b74d4e4a2692dde1b4222366
Mylife-chandafatima <zubiafatima99@gmail.com>
2025-01-18 11:59:24 +0500

    first commit

---
+++ app/product/page.tsx
@@ -0,0 +1,29 @@
+"use client"
+
+
+
+import React from 'react'
+import Link from "next/link";
+
+import {AllProduct} from '../components/allProduct'
+
+
+const page = () => {
+
+
+  return (
+      <div>
+          
+          <AllProduct />
+   
+      
+     
+     
+    
+         
+      
+    </div>
+  )
+}
+
+export default page
